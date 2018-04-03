a05d2b043e5a7c8609becefb040c2e3ed3538338
warren <microaprendizaje.org@gmail.com>
2018-04-03 12:03:43 -0500

    gg

--- common/src/main/java/org/micap/common/entity/Curso.java
+++ common/src/main/java/org/micap/common/entity/Curso.java
@@ -16,4 +16,25 @@ public class Curso {
     private String _id;
     private String name;
     public String MicroLessonId[];
+
+    public Curso setMasterId(String masterId) {
+        this.masterId = masterId;
+        return this;
+    }
+
+    public Curso set_id(String _id) {
+        this._id = _id;
+        return this;
+    }
+
+    public Curso setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Curso setMicroLessonId(String[] microLessonId) {
+        MicroLessonId = microLessonId;
+        return this;
+    }
 }
+
