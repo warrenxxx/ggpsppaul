819c927380b1e3fa10750e6882f346c9710a8695
warren <microaprendizaje.org@gmail.com>
2018-04-03 19:00:36 -0500

    gg

--- user_management/src/main/java/org/micap/user_management/UserManagementApplication.java
+++ user_management/src/main/java/org/micap/user_management/UserManagementApplication.java
@@ -1,6 +1,9 @@
 package org.micap.user_management;
 
 import org.micap.common.config.CorsConfiguration;
+import org.micap.common.entity.User;
+import org.micap.common.enums.Sexo;
+import org.micap.user_management.repository.UserDaoMongo;
 import org.micap.user_management.repository.UserDaoMongoImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,11 +12,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.server.WebFilter;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 @SpringBootApplication
 public class UserManagementApplication {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		SpringApplication.run(UserManagementApplication.class, args);
 	}
 
@@ -23,11 +28,9 @@ public class UserManagementApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(UserDaoMongoImp userDaoMongoImp){
+	public CommandLineRunner commandLineRunner(UserDaoMongo userDaoMongo){
 		return args -> {
-			System.out.println(
-					userDaoMongoImp.getUsers().collectList().block()
-			);
+		    userDaoMongo.insert(new User().setBirthDate(LocalDate.now()).setGender(Sexo.MASCULINO)).subscribe(System.out::print);
 		};
 	}
 }
